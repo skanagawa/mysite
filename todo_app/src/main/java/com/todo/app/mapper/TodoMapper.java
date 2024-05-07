25bc3f8e8b6640a1d6abac3b4088b2448aa310ec
shimpei <shimpei.tennis0515@gmail.com>
2024-05-07 09:37:33 +0900

    不明エラー

---
+++ todo_app/src/main/java/com/todo/app/mapper/TodoMapper.java
@@ -0,0 +1,14 @@
+package com.todo.app.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.todo.app.entity.Todo;
+
+@Mapper
+public interface TodoMapper {
+
+    public List<Todo> selectAll();
+}
+
